74d80734686e74417494f0cbbec861d967529557
Franco Marinozzi <134655876+francomarinozzi@users.noreply.github.com>
2024-11-09 10:44:25 -0300

    update

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -3,11 +3,9 @@ const mongoose = require("mongoose");
 const validarSchema = (schema) => {
     return async (req, res, next) => {
         try {
-            // Validar el cuerpo de la solicitud usando el esquema
             await schema.validateAsync(req.body);
-            next();  // Si la validación pasa, se pasa al siguiente middleware o controlador
+            next();  
         } catch (error) {
-            // En caso de error, se captura y se responde con el mensaje de error
             return res.status(400).json({ message: error.message });
         }
     };
